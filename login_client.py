3655d7f34cd9c52451325915b0750b8a342d8fdd
Alejandro Bernal <alexbernal32396@gmail.com>
2018-07-09 15:37:08 -0700

    Made the server and client code pep 8 compliant using PEP 8 online style check

--- login_client.py
+++ login_client.py
@@ -1,41 +1,45 @@
 import socket
-#make PEP-8 Compliant create a separate function for login 
+"""
+    TODO: Create a menu function that asks the user to do something
+"""
+
 
 def establish_Connection():
-	#Login here
-	#TODO: add a loop to ask the user to have correct credentials
-	#connect to the initiated server
-	s = socket.socket()
-	s.connect(('127.0.0.1', 12345))
-	msg = s.recv(1024).decode()
-	print(msg)
-	login(s)
-	
+    # connect to the initiated server
+    s = socket.socket()
+    s.connect(('127.0.0.1', 12345))
+    msg = s.recv(1024).decode()
+    print(msg)
+    login(s)
+
+
 def login(s):
-	#This is the users first time, ask for credentials
-	logged_in = False
-	while not logged_in:
-		#login to the existing accounts on server
-		print("Welcome, please Login")
-		username = input("Username: ")
-		password = input("Password: ")
-		s.sendall(username.encode())
-		s.sendall(password.encode())
-		server_rsp = s.recv(1024).decode()
-		if "Welcome" in server_rsp:
-			logged_in = True
-			print(server_rsp)
-			break
-		else:
-			logged_in = False
-			print(server_rsp)
-			continue
-	else:
-		print(f"You are logged in as {username}")
-		s.close()
+    # This is the users first time, ask for credentials
+    logged_in = False
+    while not logged_in:
+        # login to the existing accounts on server
+        print("Welcome, please Login")
+        username = input("Username: ")
+        password = input("Password: ")
+        s.sendall(username.encode())
+        s.sendall(password.encode())
+        server_rsp = s.recv(1024).decode()
+        if "Welcome" in server_rsp:
+            logged_in = True
+            print(server_rsp)
+            break
+        else:
+            logged_in = False
+            print(server_rsp)
+            continue
+    else:
+        print(f"You are logged in as {username}")
+        s.close()
+
 
 def main():
-	establish_Connection()
+    establish_Connection()
+
 
 if __name__ == "__main__":
-	main()		
+    main()
--- login_server.py
+++ login_server.py
@@ -1,50 +1,53 @@
 import socket
-#TODO: add a loop to ask the user to have correct credentials 
-#TODO: loop to return back to login once logged out 
-#TODO: add registration 
-#TODO: make PEP-8 Compliant
+"""
+    TODO: loop to return back to login once logged out
+    TODO: add registration
+"""
+# create a list of valid users and passwords
+LOGINS = [
+    ("Alejandro", "pythonrules"),
+    ("Betty", "ilovethebackstreetboys"),
+    ("Maria", "postmalone"),
+    ("William", "pinkfloyd")
+]
 
-#Create a list of valid users and passwords
-LOGINS = [("Alejandro", "pythonrules"),
-		  ("Betty", "ilovethebackstreetboys"),
-		  ("Maria", "postmalone"),
-		  ("William", "pinkfloyd")
-		  ]
-		  
+
 def initiate_Server():
-	#Listen for connections
-	s = socket.socket()
-	s.bind(("127.0.0.1", 12345))
-	s.listen(5)
-	print('Server Listening...')
-	print("Waiting for Connection")
-	
-	#establish a connection with the client.py
-	conn, addr = s.accept()
-	print(f"Got connection from: {addr}")
-	msg = "A Connection has been established"
-	conn.sendall(msg.encode())
-	logged_in = False
-	while not logged_in:
-		logged_in = validate_Login(conn)	
-	conn.close()
+    # Listen for connections
+    s = socket.socket()
+    s.bind(("127.0.0.1", 12345))
+    s.listen(5)
+    print('Server Listening...')
+    print("Waiting for Connection")
+    # establish a connection with the client.py
+    conn, addr = s.accept()
+    print(f"Got connection from: {addr}")
+    msg = "A Connection has been established"
+    conn.sendall(msg.encode())
+    # check if the client is logged in or not
+    logged_in = False
+    while not logged_in:
+        logged_in = validate_Login(conn)
+    conn.close()
+
 
 def validate_Login(conn):
-	#receive the input given by user to check if it is valid login
-	username = conn.recv(1024).decode()
-	password = conn.recv(1024).decode()
-	
-	if (username, password) in LOGINS:
-		rsp = f"Welcome! {username}"
-		conn.sendall(rsp.encode())
-		return True
-	else:
-		rsp = "Not a valid username or password"
-		conn.sendall(rsp.encode())
-	return False	
-	
+    # receive the input given by user to check if it is valid login
+    username = conn.recv(1024).decode()
+    password = conn.recv(1024).decode()
+    if (username, password) in LOGINS:
+        rsp = f"Welcome! {username}"
+        conn.sendall(rsp.encode())
+        return True
+    else:
+        rsp = "Not a valid username or password"
+        conn.sendall(rsp.encode())
+    return False
+
+
 def main():
-	initiate_Server()
+    initiate_Server()
+
 
 if __name__ == "__main__":
-	main()	
+    main()
