927cb6c3e875442392ed458152b5c8a8c109a5ad
Alejandro Bernal <alexbernal32396@gmail.com>
2018-07-10 16:26:57 -0700

    Added menu options at start of the application. The user can now choose to login or exit.

--- login_client.py
+++ login_client.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 """
     TODO: Create a menu function that asks the user to do something
 """
@@ -34,25 +35,36 @@ def login(s):
 	else:
 		print(f"You are logged in as {username}")
 		return s, logged_in, username
-		s.close()
+	s.close()
 
 
 def menu(s, logged_in, username):
 	# once the user logs in this should give them options: Logout
-	#TODO: add extra options including Register
-	while logged_in:
+	#TODO: implement this menu function when the user logs in 
+	while not logged_in:
 		print(f"What would you like to do {username}?")
-		break
 	else:
 		print("Please login")
 		
 		
 def main():
 	#TODO: fix logged_in make it its own function 
-	s = establish_connection()
-	login(s)
-	menu(s, logged_in, username)
+	# Start the application show menu with options
+	print("Welcome to the App\n Menu: Login | Register | Exit")
+	# Give the use the ability to choose from the options
+	while True:
+		menu_opt = input(">>").lower()
+		if menu_opt == "login":
+			# Establish the connection to the server using the socket
+			s = establish_connection()
+			login(s)
+			break
+		elif menu_opt == "exit":
+			print("Bye See you soon!")
+			sys.exit()
+		else:
+			print(f"{menu_opt} is not a valid menu option.")
 
-
+			
 if __name__ == "__main__":
     main()
